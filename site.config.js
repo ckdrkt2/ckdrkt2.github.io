53a00808dd2cde8a2d2363bfcce1758d73bb3841
ckdrkt2 <bbiuyy@gmail.com>
2023-07-18 04:27:25 +0000

    update avatar

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Changshin Yoo",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/profile.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "MLOps Engineer",
     bio: "Hi!",
     email: "bbiuyy@gmail.com",
