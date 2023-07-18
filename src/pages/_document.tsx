af9be6a13d71e58d58cbad655f4b3914d75d758b
ckdrkt2 <bbiuyy@gmail.com>
2023-07-18 04:51:02 +0000

    fix favicon size

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
             as="font"
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
           />
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="16x16"/>
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
           <link
             rel="apple-touch-icon"
             sizes="192x192"
