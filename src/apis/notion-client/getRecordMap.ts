368b34bd366d4d2b5a2b907fc18dfc958324c62b
ckdrkt2 <bbiuyy@gmail.com>
2024-12-30 01:51:10 +0900

    test

--- src/apis/notion-client/getRecordMap.ts
+++ src/apis/notion-client/getRecordMap.ts
@@ -3,6 +3,7 @@ import { NotionAPI } from "notion-client"
 export const getRecordMap = async (pageId: string) => {
   const api = new NotionAPI()
   const recordMap = await api.getPage(pageId)
+  console.log(123123123)
   console.log(recordMap)
   return recordMap
 }
